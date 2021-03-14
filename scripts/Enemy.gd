c08b6b794de860c5067dfe8a8b9ac46f1f630ba0
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-03-14 12:46:39 -0400

    started work on enemy script, split up the sprites again

---
+++ scripts/Enemy.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+class_name Enemy
+
+
+
+var type
+
+func _init(new_type):
+	self.type = new_type
+	pass
+
+
+func process_turn():
+	return
---
+++ scripts/Enemy_Type.gd
@@ -0,0 +1,248 @@
+class_name Enemy_Type
+
+const avatar = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile200.png"
+}
+const goblin = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile201.png"
+}
+const goblin_wizard = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile202.png"
+}
+const monkey = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile203.png"
+}
+const robber = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile204.png"
+}
+const human_wizard = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile205.png"
+}
+const human_sorcerer = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile206.png"
+}
+const human_jack = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile207.png"
+}
+const human_queen = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile208.png"
+}
+const human_king = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile209.png"
+}
+const rat = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile210.png"
+}
+const bat = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile211.png"
+}
+const dog = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile212.png"
+}
+const snake = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile213.png"
+}
+const spider_tiny = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile214.png"
+}
+const spider_big = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile215.png"
+}
+const eye = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile216.png"
+}
+const slime_tiny = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile217.png"
+}
+const slime_big = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile218.png"
+}
+const vines = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile219.png"
+}
+const slime_boss = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile220.png"
+}
+const goblin_boss = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile221.png"
+}
+const undead = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile222.png"
+}
+const demon = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile223.png"
+}
+const vampire = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile224.png"
+}
+const lich = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile225.png"
+}
+const bull_god = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile226.png"
+}
+const golem_god = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon":"res://assets/visuals/tile227.png"
+}
+const alien_god = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile228.png"
+}
+const dragon_god = {
+	"base_health": 10,
+	"base_attack": 10,
+	"base_speed": 10,
+	"base_loot": 10,
+	"icon": "res://assets/visuals/tile229.png"
+}
+
+
+
+const enemy_types = {
+	"avatar": avatar,
+	"goblin": goblin,
+	"goblin_wizard": goblin_wizard,
+	"monkey": monkey,
+	"robber": robber,
+	"human_wizard": human_wizard,
+	"human_sorcerer": human_sorcerer,
+	"human_jack": human_jack,
+	"human_queen": human_queen,
+	"human_king": human_king,
+	"rat": rat,
+	"bat": bat,
+	"dog": dog,
+	"snake": snake,
+	"spider_tiny": spider_tiny,
+	"spider_big": spider_big,
+	"eye": eye,
+	"slime_tiny": slime_tiny,
+	"slime_big": slime_big,
+	"vines": vines,
+	"slime_boss": slime_boss,
+	"goblin_boss": goblin_boss,
+	"undead": undead,
+	"demon": demon,
+	"vampire": vampire,
+	"lich": lich,
+	"bull_god": bull_god,
+	"golem_god": golem_god,
+	"alien_god": alien_god,
+	"dragon_god": dragon_god
+}
+
