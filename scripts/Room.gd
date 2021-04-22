e4ccf9bb1f928d4fef6d545aacaa1ca6f48abd2f
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-04-22 16:52:59 -0400

    started work on the room class

--- scripts/Room.gd
+++ scripts/Room.gd
@@ -2,30 +2,60 @@ extends Node2D
 
 class_name Room
 
-var generic_room_types = Room_Type.new()
-var type_enum
-var theme_enum
-var type_stats
-var theme_stats
+#this for the sprite frame
+const room_theme_dict = {
+	Tile_Enums.tile_themes_enum.castle: 0,
+	Tile_Enums.tile_themes_enum.forest: 1,
+	Tile_Enums.tile_themes_enum.grave: 2,
+	Tile_Enums.tile_themes_enum.mountain: 3,
+	Tile_Enums.tile_themes_enum.swamp: 4
+}
 
+#[base enemies to gen, base loot to gen, base number of shopkeep items, can return to room true1/false0, can rest true1/false0]
+const room_type_dict = {
+	Tile_Enums.center_type_enum.battle: [1, 1, 0, 0, 0],
+	Tile_Enums.center_type_enum.treasure: [0, 1, 0, 1, 0],
+	Tile_Enums.center_type_enum.shop: [0, 0, 1, 1, 0],
+	Tile_Enums.center_type_enum.rest: [0, 0, 0, 0, 1],
+	Tile_Enums.center_type_enum.silly: [0, 0, 0, 0, 0]
+}
+
+
+
+var type_enum
+var room_type_hash #stores the stats about the room type from the room_type_dict
+var theme_enum
+var room_theme_frame #stores the frame number of the room theme
+var room_screen_loc #the location the room will appear on the player's screen
+
+var ani_sprite
 
 func _ready():
-	generate_room_stats()
-
-func _init(new_type, new_theme):
-	self.type_enum = new_type
-	self.theme_enum = new_theme
-
-func generate_room_stats():
-	if self.type_enum == null:
-		self.type_enum = generic_room_types.room_types_enum
-		self.theme_enum = generic_room_types.tile_themes_enum
-	self.type_stats = generic_room_types.room_types_dict.get(self.type_enum)
-	self.theme_stats = generic_room_types.room_themes_dict.get(self.theme_enum)
-	print(self.type_stats.get("name"))
-
-func generate_room():
+	ani_sprite = AnimatedSprite.new()
+	ani_sprite.set_sprite_frames(load("res://assets/visuals/room_bg_frames.tres"))
+	ani_sprite.set_frame(room_theme_frame)
+	position = room_screen_loc
+	generate_room()
 	return
 
-func player_access_room():
+func _init(new_type, new_theme, new_room_screen_loc):
+	if new_theme in room_theme_dict:
+		theme_enum = new_theme
+	else:
+		theme_enum = Tile_Enums.tile_themes_enum.castle
+	if new_type in room_type_dict:
+		type_enum = new_type
+	else:
+		type_enum = Tile_Enums.center_type_enum.silly
+	room_type_hash = room_type_dict[type_enum]
+	room_theme_frame = room_theme_dict[theme_enum]
+	room_screen_loc = new_room_screen_loc
+	return
+
+func generate_room():
+	#unhide anisprite
+	add_child(ani_sprite)
+	return
+
+func leave_room():
 	return
