98d5a6804ff06910d8b15e2604cbd0f0f9d5c142
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-05-15 20:08:28 -0400

    started work on the item enums and class

---
+++ scripts/Item_Enums.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+class_name Item_Enums
+
+enum item_types{
+	armor,
+	weapon,
+	jewelry,
+	spell,
+	healing,
+	consumable
+}
+
+enum quality_types{
+	trash,
+	common,
+	uncommon,
+	rare,
+	unique,
+	masterwork,
+	legendary
+}
